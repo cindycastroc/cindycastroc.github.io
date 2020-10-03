677c9bc7fbdbfc1ff7555473cc1397b998178ee9
cindycastroc <66134539+cindycastroc@users.noreply.github.com>
2020-10-03 10:37:23 -0500

    Links updated

--- behavior.js
+++ behavior.js
@@ -1,1 +1,6 @@
-document.getElementById('modified').innerHTML = document.lastModified;
+months = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+var theDate = new Date(document.lastModified); 
+theDate.setTime((theDate.getTime()+(5000*60*60)) ) 
+with (theDate) { 
+document.write(+getDate()+' '+months[getMonth()]+' '+getYear()+' '+getHours()+':'+getMinutes()+" GMT</i>") 
+} 
