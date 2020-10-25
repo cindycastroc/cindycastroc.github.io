9d9c48af46fe1e55612d4d235a67e1854a8ccfb7
cindycastroc <66134539+cindycastroc@users.noreply.github.com>
2020-10-24 23:10:58 -0500

    Preston6First

--- lesson5/js/behavior.js
+++ lesson5/js/behavior.js
@@ -4,3 +4,10 @@ const navbar = document.querySelector(".navbar");
 menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("change");
 });
+
+months = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+var theDate = new Date(document.lastModified); 
+theDate.setTime((theDate.getTime()+(5000*60*60)) ) 
+with (theDate) { 
+document.write(+getDate()+' '+months[getMonth()]+' '+getYear()+' '+getHours()+':'+getMinutes()+" GMT</i>") 
+} 
