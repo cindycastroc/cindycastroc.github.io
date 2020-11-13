f79b5d430c1c25665718601a9f23b6675051c891
cindycastroc <cindy.castro.chavez@gmail.com>
2020-11-12 23:26:36 -0600

    second

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -4,17 +4,28 @@ fetch(requestURL)
     return response.json();
   })
   .then(function (jsonObject) {
-    console.table(jsonObject);
+    console.table(jsonObject); // this will be temporary
     for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
+        let birthDate = document.createElement('p')
+        let birthPlace = document.createElement('p')
+        let externalImage = document.createElement('img')
 
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
+        birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
+        birthPlace.textContent = 'Place of Birth: ' + prophets[i].birthplace;
+        externalImage.setAttribute('src', prophets[i].imageurl);
+        externalImage.setAttribute('alt', h2.textContent + '-' + prophets[i].order);
+        externalImage.setAttribute('height', '205');
 
         card.appendChild(h2);
+        card.appendChild(birthDate);
+        card.appendChild(birthPlace);
+        card.appendChild(externalImage);
 
         document.querySelector('div.cards').appendChild(card);
-      }  // temporary checking for valid response and data parsing
+      }  
   });
 
   const prophets = jsonObject['prophets'];
