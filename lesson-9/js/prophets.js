a296ec7f11554a738448f0efb74b47f453bf7b12
cindycastroc <cindy.castro.chavez@gmail.com>
2020-11-12 23:34:09 -0600

    fourth

--- lesson-9/js/prophets.js
+++ lesson-9/js/prophets.js
@@ -11,9 +11,9 @@ fetch(requestURL)
     for (let i = 0; i < prophets.length; i++ ) {
         let card = document.createElement('section');
         let h2 = document.createElement('h2');
-        let birthDate = document.createElement('p')
-        let birthPlace = document.createElement('p')
-        let externalImage = document.createElement('img')
+        let birthDate = document.createElement('p');
+        let birthPlace = document.createElement('p');
+        let externalImage = document.createElement('img');
 
         h2.textContent = prophets[i].name + ' ' + prophets[i].lastname;
         birthDate.textContent = 'Date of Birth: ' + prophets[i].birthdate;
