c68a7fc532b3c95a2c9ee623cb3aa35e6a8f123c
cindycastroc <cindy.castro.chavez@gmail.com>
2020-11-16 12:04:19 -0600

    General Uptades

--- lesson4/js/behavior.js
+++ lesson4/js/behavior.js
@@ -1,6 +1,16 @@
+// NAVBAR
 const menuIcon = document.querySelector(".hamburger-menu");
 const navbar = document.querySelector(".navbar");
 
 menuIcon.addEventListener("click", () => {
     navbar.classList.toggle("change");
 });
+
+
+// DATE
+months = ['January', 'Febraury', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']; 
+var theDate = new Date(document.lastModified); 
+theDate.setTime((theDate.getTime()+(5000*60*60)) ) 
+with (theDate) { 
+document.write(+getDate()+' '+months[getMonth()]+' '+getYear()+' '+getHours()+':'+getMinutes()+" GMT</i>") 
+} 
