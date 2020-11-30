d8be8e853b221412b014756c15f70152488bdb4b
cindycastroc <cindy.castro.chavez@gmail.com>
2020-11-30 11:22:35 -0600

    .

--- WEATHER SITE/PRESTON/js/summary.js
+++ WEATHER SITE/PRESTON/js/summary.js
@@ -1,5 +1,4 @@
 const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=5604473&units=imperial&APPID=79e15b681560d53f708b95aafeb82274`;
-// https://api.openweathermap.org/data/2.5/weather?id=5604473&appid=3efcd4225e37daee266d0b251f791a4b&units=imperial
 fetch(apiURL)
     .then((response) => response.json())
     .then((jsObject) => {
@@ -7,20 +6,7 @@ fetch(apiURL)
         document.getElementById('temp').textContent = jsObject.weather[0].description;
         document.getElementById('hum').textContent = jsObject.main.humidity;
         document.getElementById('wind-speed').textContent = jsObject.wind.speed;
-        document.getElementById('currently').textContent = jsObject.weather[0].main;
+        document.getElementById('currently').textContent = Math.round(jsObject.main.temperature);
     
     let temp = jsObject.
-});
-
-
-
-const curtemp = document.querySelector('#current-temp');
-const weathericon = document.querySelector('#icon')
-const iconsource = document.querySelector('#imagesrc')
-curtemp.innerHTML = jsObject.main.temp;
-
-iconsource.textContent = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-const isrc = `https://openweathermap.org/img/w/${jsObject.weather[0].icon}.png`;
-
-weathericon.setAttribute('src', isrc)
-weathericon.setAttribute('alt', jsObject.weather[0].description);
+    });
